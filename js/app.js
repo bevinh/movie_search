1897dad19e477ca91063af151acd22f558a47813
bevinh <admin_bah34@CMFR-143.local>
2016-12-31 14:38:54 -0500

    Made the second call to IMDB

--- js/app.js
+++ js/app.js
@@ -1,5 +1,4 @@
 var App = (function(){
-
 $(".search-form").submit(function(evt){
     evt.preventDefault();
     var searchText = $("#search").val();
@@ -16,27 +15,74 @@ $(".search-form").submit(function(evt){
         page: "1",
         callback: ""
     };
+var descHTML =  '<div id="descHTML"><div class="topbar-description"><div id="backNav"><i class="material-icons white">keyboard_arrow_left</i><span class="small-text white">Search Results</span></div>';
 
-    $.getJSON(url, data, function(data){
+$.getJSON(url, data, function(data){
         if(data.Search) {
             $.each(data.Search, function (id, movie) {
                 console.log(data.Search);
                     if (movie.Poster === "N/A") {
-                        listingHTML += "<li><a href='http://www.imdb.com/title/" + movie.imdbID + "'><div class='poster-wrap'><i class='material-icons poster-placeholder'>crop_original</i></div>";
+                        listingHTML += "<li id='" + movie.imdbID + "'><div class='poster-wrap'><i class='material-icons poster-placeholder'>crop_original</i></div>";
                     } else {
-                        listingHTML += "<li><a href='http://www.imdb.com/title/" + movie.imdbID + "'><div class='poster-wrap'><img class='movie-poster' src='" + movie.Poster + "'/></div>";
-
+                        listingHTML += "<li id='" + movie.imdbID + "'><div class='poster-wrap'><img class='movie-poster' src='" + movie.Poster + "'/></div>";
                     }
+
 
                     listingHTML += "<span class='movie-title'>" + movie.Title + "</span>";
                     listingHTML += "<span class='movie-year'>" + movie.Year + "</span>";
                     listingHTML += "</a></li>";
+
+                //setting up the html for the description
+                    //
+
             });
         } else {
             listingHTML += "<li class='no-movies'><i class='material-icons icon-help'>help_outline</i>No movies found that match: " + searchText + ".</li>"
         }
+
         $("#movies").append(listingHTML);
+        function getDescription (){
+            var movie = $(this).attr('id');
+            $("#descHTML").empty();
+            var descData = {
+                i: movie,
+                type: "Movie",
+                r: "json",
+                page: "1",
+                callback: ""
+            }
+
+            $.getJSON(url, descData, function(movie) {
+                if (movie.Poster === "N/A") {
+                    descHTML += '<div class="wrap-small-poster"><div class="poster-wrap small-poster"><i class="material-icons poster-placeholder">crop_original</i></div></div>';
+                } else {
+                    descHTML += '<div class="wrap-small-poster"><div class="poster-wrap small-poster"><img src="' + movie.Poster + '"/></div></div>';
+                }
+                descHTML +=  '<div class="description-text"><div><span class="title-description white">' + movie.Title + '(' + movie.Year + ')</span></div><div><span class="imdb-rating gray">IMDB Rating ' + movie.imdbRating + '</span></div></div>'
+                descHTML += '</div><div class="wrap"><div class="synopsis"><span class="synopsis-title">Plot Synopsis:</span>';
+                descHTML += '<p class="synopsis-text">' + movie.Plot + '</p>';
+                descHTML += '<button class="button-imdb">View on IMDB</button></div>';
+                $("#movies").hide();
+                $(".main-content").append(descHTML);
+                function returnToSearch(){
+                    $("#descHTML").remove();
+                    $("#movies").show();
+                    movie = '';
+                    //TODO: Figure out why it's not clearing the data
+                }
+                document.getElementById("backNav").addEventListener('click', returnToSearch);
+            });
+
+            //$('#backNav').addEventListener('click', returnToSearch);
+        }
+
+        $('li').each(function(index){
+            this.addEventListener('click', getDescription);
+        });
     });
+
 });
+
+
     return App;
 })();
