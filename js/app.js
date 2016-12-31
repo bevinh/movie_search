805faba33550c07c4b06c6f05c0030ea3e459de1
bevinh <admin_bah34@CMFR-143.local>
2016-12-31 10:31:00 -0500

    Added the links to the IMDB database

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-(function(){
+var App = (function(){
 
 $(".search-form").submit(function(evt){
     evt.preventDefault();
@@ -8,7 +8,7 @@ $(".search-form").submit(function(evt){
     //empty out the movies to prevent search results appending endlessly
     $("#movies").empty();
     $(".desc").remove();
-    //setting up the data for the movie
+    //set up the data for the movie
     var data = {
         s: searchText,
         type: "Movie",
@@ -16,20 +16,21 @@ $(".search-form").submit(function(evt){
         page: "1",
         callback: ""
     };
+
     $.getJSON(url, data, function(data){
-        console.log(data.Search);
         if(data.Search) {
             $.each(data.Search, function (id, movie) {
+                console.log(data.Search);
                     if (movie.Poster === "N/A") {
-                        listingHTML += "<li><div class='poster-wrap'><i class='material-icons poster-placeholder'>crop_original</i></div>";
+                        listingHTML += "<li><a href='http://www.imdb.com/title/" + movie.imdbID + "'><div class='poster-wrap'><i class='material-icons poster-placeholder'>crop_original</i></div>";
                     } else {
-                        listingHTML += "<li><div class='poster-wrap'><img class='movie-poster' src='" + movie.Poster + "'/></div>";
+                        listingHTML += "<li><a href='http://www.imdb.com/title/" + movie.imdbID + "'><div class='poster-wrap'><img class='movie-poster' src='" + movie.Poster + "'/></div>";
 
                     }
 
                     listingHTML += "<span class='movie-title'>" + movie.Title + "</span>";
                     listingHTML += "<span class='movie-year'>" + movie.Year + "</span>";
-                    listingHTML += "</li>";
+                    listingHTML += "</a></li>";
             });
         } else {
             listingHTML += "<li class='no-movies'><i class='material-icons icon-help'>help_outline</i>No movies found that match: " + searchText + ".</li>"
@@ -37,5 +38,5 @@ $(".search-form").submit(function(evt){
         $("#movies").append(listingHTML);
     });
 });
-
+    return App;
 })();
